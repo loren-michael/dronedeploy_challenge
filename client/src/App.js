5b84cad9c0dfb198ec2441961e3cd237336153d5
loren <lorenmichaelh@gmail.com>
2024-11-07 18:59:03 -0600

    Building out basic openai chat bot

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import Prompt from './Prompt';
+
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then(r => r.json())
-      .then(data => setCount(data.count))
-  }, [])
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+      <h1>DroneDeploy Challenge</h1>
+      <Prompt />
     </div>
   )
 }
--- client/src/Prompt.js
+++ client/src/Prompt.js
@@ -1,8 +1,88 @@
 import React from 'react'
+import { useState } from 'react';
+import OpenAI from 'openai';
+
+// "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"
+
+const openai = new OpenAI({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true
+})
+
 
 const Prompt = () => {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    setIsTyping(true);
+    let msgs = chats;
+    msgs.push({role:"user", content: message})
+    setChats(msgs)
+    setMessage("");
+
+    await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are EbereGPT. You help with image data."
+        },
+        ...chats,
+      ],
+    }).then(r => {
+      console.log(r)
+      msgs.push(r.choices[0].message);
+      setChats(msgs);
+      setIsTyping(false);
+    }).catch(error => console.log(error))
+  };
+
+
   return (
-    <div>Prompt</div>
+    <div>
+      <h2>
+        ChatGPT App
+      </h2>
+
+      <section>
+        {
+          chats && chats.length ? (
+            chats.map((chat, index) => {
+              return (
+                <p key={index} className={chat.role === "user" ? "user_msg" : ""} >
+                  <span>{chat.role}</span>
+                  <span>:  </span>
+                  <span>{chat.content}</span>
+                </p>)
+            })
+          ) : ""
+        }
+      </section>
+
+    {
+      isTyping ? (
+        <div>
+          <p>
+            <i>Thinking...</i>
+          </p>
+        </div>
+      ) : ""
+    }
+
+      <form onSubmit={e => chat(e, message)}>
+        <input 
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type a message and hit enter" 
+          onChange={e => setMessage(e.target.value)}
+        />
+      </form>
+    </div>
   )
 }
 
